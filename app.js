7340deda10ff7ccef43c6766af881dc434f1e48b
Tom <tmelliottjr@gmail.com>
2018-04-26 09:57:04 -0400

    websocket only

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ let connections = {};
 //app.use(cors());
 //app.options('*', cors());
 
+io.set('transports', ['websocket']);
+
 server.listen(port, () => {
   console.log(`Server running on localhost:${port}`)
 })

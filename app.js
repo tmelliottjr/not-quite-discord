514b521b886b311b4af5cf7c52198e369e7bd231
Tom <tmelliottjr@gmail.com>
2018-04-26 08:41:41 -0400

    add cors preflight

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const server = require('http').createServer(app);
-//const io = require('socket.io')(server);
+const io = require('socket.io')(server);
 const sanitize = require('./helpers/sanitize')
 const port = 5000;
 
@@ -13,7 +13,8 @@ const port = 5000;
 let connections = {};
 
 // Allow CORS
-//app.use(cors());
+app.use(cors());
+app.options('*', cors());
 
 server.listen(5000, () => {
   console.log(`Server running on localhost:${port}`)
@@ -24,67 +25,67 @@ app.get('/connections', (req, res) =>{
   res.send(connections);
 });
 
-// io.on('connect', connectionHandler);
+io.on('connect', connectionHandler);
 
-// function connectionHandler(client) {
-//   let name = client.handshake.query.name;
-//   console.log(`${name} has connected.`);
+function connectionHandler(client) {
+  let name = client.handshake.query.name;
+  console.log(`${name} has connected.`);
 
-//   let nameFound = Object.values(connections).findIndex(ele => {
-//     return ele === name;
-//   });
+  let nameFound = Object.values(connections).findIndex(ele => {
+    return ele === name;
+  });
 
-//   if (nameFound > -1) {
-//     console.log('Name already taken. Notifying client and disconnecting session.');
-//     client.emit('connection-error', 'Name already taken.');
-//     client.disconnect();
-//     return;
-//   }
+  if (nameFound > -1) {
+    console.log('Name already taken. Notifying client and disconnecting session.');
+    client.emit('connection-error', 'Name already taken.');
+    client.disconnect();
+    return;
+  }
 
-//   connections[client.id] = name;
+  connections[client.id] = name;
 
-//   let payload = [
-//     name,
-//     connections
-//   ]
+  let payload = [
+    name,
+    connections
+  ]
 
-//   console.log('Successful client connections.');
-//   client.emit('connection-success');
-//   console.log('Broadcasting connection to all users.');
-//   io.sockets.emit('participant-connected', payload);
+  console.log('Successful client connections.');
+  client.emit('connection-success');
+  console.log('Broadcasting connection to all users.');
+  io.sockets.emit('participant-connected', payload);
 
-//   client.on('disconnect', function(reason){
-//     disconnectHandler(client, reason);
-//   });
+  client.on('disconnect', function(reason){
+    disconnectHandler(client, reason);
+  });
 
-//   client.on('client-message', function(data){
-//     messageHandler(client, data);
-//   });
+  client.on('client-message', function(data){
+    messageHandler(client, data);
+  });
 
-// }
+}
 
-// function disconnectHandler(client, reason) {
+function disconnectHandler(client, reason) {
 
-//   let name = connections[client.id]
+  let name = connections[client.id]
 
-//   delete connections[client.id]
+  delete connections[client.id]
 
-//   if (!name) return
+  if (!name) return
 
-//   let payload = [
-//     name,
-//     connections
-//   ]
+  let payload = [
+    name,
+    connections
+  ]
   
-//   io.emit('user-disconnected', payload)
+  io.emit('user-disconnected', payload)
 
-// }
+}
 
-// function messageHandler(client, data) {
+function messageHandler(client, data) {
 
-//   let name = connections[client.id]
+  let name = connections[client.id]
 
-//   io.emit('message', {'name': name, 'message': sanitize(data)})
-// }
+  io.emit('message', {'name': name, 'message': sanitize(data)})
+}
 
 

5277287af8550f2b11b93d4c34a390404beb72d9
Tom <tmelliottjr@gmail.com>
2018-04-26 10:56:36 -0400

    remove debug logging; update sylting

--- app.js
+++ app.js
@@ -12,32 +12,27 @@ const port = 5000;
 let connections = {};
 
 app.use(cors());
-//app.options('*', cors());
 
 io.transports = ['websocket'];
 
 server.listen(port, () => {
-  console.log(`Server running on localhost:${port}`)
+  console.log(`Server running on localhost:${port}`);
 })
 
 app.get('/connections', (req, res) =>{
-  console.log('Getting connections.');
   res.send(connections);
-  return;
 });
 
 io.on('connect', connectionHandler);
 
 function connectionHandler(client) {
   let name = client.handshake.query.name;
-  console.log(`${name} has connected.`);
 
   let nameFound = Object.values(connections).findIndex(ele => {
     return ele === name;
   });
 
   if (nameFound > -1) {
-    console.log('Name already taken. Notifying client and disconnecting session.');
     client.emit('connection-error', 'Name already taken.');
     client.disconnect();
     return;
@@ -48,11 +43,10 @@ function connectionHandler(client) {
   let payload = [
     name,
     connections
-  ]
+  ];
 
-  console.log('Successful client connections.');
   client.emit('connection-success');
-  console.log('Broadcasting connection to all users.');
+
   io.sockets.emit('participant-connected', payload);
 
   client.on('disconnect', function(reason){
@@ -67,26 +61,24 @@ function connectionHandler(client) {
 
 function disconnectHandler(client, reason) {
 
-  let name = connections[client.id]
+  let name = connections[client.id];
 
-  delete connections[client.id]
+  delete connections[client.id];
 
-  if (!name) return
+  if (!name) return;
 
   let payload = [
     name,
     connections
-  ]
+  ];
   
-  io.emit('user-disconnected', payload)
+  io.emit('user-disconnected', payload);
 
 }
 
 function messageHandler(client, data) {
-
-  let name = connections[client.id]
-
-  io.emit('message', {'name': name, 'message': sanitize(data)})
+  let name = connections[client.id];
+  io.emit('message', {'name': name, 'message': sanitize(data)});
 }
 
 
